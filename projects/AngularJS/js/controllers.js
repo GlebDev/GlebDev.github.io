82a0e6d6acc916e91cf39abaa7c597b1d7e446d2
GlebDev <webdev.glib@gmail.com>
2018-03-06 18:48:14 +0200

    add AssetBundles

--- projects/AngularJS/js/controllers.js
+++ projects/AngularJS/js/controllers.js
@@ -1,37 +1,33 @@
  'use strict'
-//
-// Controllers
-
-var phonecatApp = angular.module('phonecatApp',[]);
-
-phonecatApp.controller("PhoneListCtrl",function($scope,$http){
-    $http.get('phones/phones.json').success(function(data,status,headers,config){
-        console.log('This is Data:',data,'\n\n This is Status:',status,'\n\n This is Headers:',headers,'\n\n This is Config:',config);
-        $scope.phones = data;
-    }).error(function(){
-
-    });
-    $scope.title = "Phones";
-    var date = new Date();
-    $scope.today = date;
-    $scope.doneAndFilter = function(phoneItem) {
-        return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true;
-    }
-    $scope.sortField = undefined;
-    $scope.reverse = false;
-
-    $scope.sort = function(fieldName){
-        if($scope.sortField === fieldName){
-            $scope.reverse = !$scope.reverse;
-        } else {
-            $scope.sortField = fieldName;
-            $scope.reverse = !$scope.reverse;
-        }
-    };
-    $scope.isSortUp = function(fieldName){
-        return $scope.sortField === fieldName && !$scope.reverse;
-    };
-    $scope.isSortDown = function(fieldName){
-        return $scope.sortField === fieldName && $scope.reverse;
-    };
-});
+ //
+ // Controllers
+ var phonecatApp = angular.module('phonecatApp', []);
+ phonecatApp.controller("PhoneListCtrl", function ($scope, $http) {
+     $http.get('phones/phones.json').success(function (data, status, headers, config) {
+         console.log('This is Data:', data, '\n\n This is Status:', status, '\n\n This is Headers:', headers, '\n\n This is Config:', config);
+         $scope.phones = data;
+     }).error(function () {});
+     $scope.title = "Phones";
+     var date = new Date();
+     $scope.today = date;
+     $scope.doneAndFilter = function (phoneItem) {
+         return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true;
+     }
+     $scope.sortField = undefined;
+     $scope.reverse = false;
+     $scope.sort = function (fieldName) {
+         if ($scope.sortField === fieldName) {
+             $scope.reverse = !$scope.reverse;
+         }
+         else {
+             $scope.sortField = fieldName;
+             $scope.reverse = !$scope.reverse;
+         }
+     };
+     $scope.isSortUp = function (fieldName) {
+         return $scope.sortField === fieldName && !$scope.reverse;
+     };
+     $scope.isSortDown = function (fieldName) {
+         return $scope.sortField === fieldName && $scope.reverse;
+     };
+ });
